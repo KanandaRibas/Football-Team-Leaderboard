32d60734f4a7b6787c4bb0031a0d7d77d4a43bc5
Kananda Ribas <kanandaribas@gmail.com>
2023-05-12 20:43:20 -0300

    feat: req6 - Cria model e migration para a tabela de pessoas usuárias

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from './index';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
