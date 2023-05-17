90a93cac19395504d55ff01e0bb60c3024cb92c3
Kananda Ribas <kanandaribas@gmail.com>
2023-05-16 21:32:18 -0300

    test: Inicia testes do fluxo matches

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,53 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { Model } from 'sequelize'
+import Match from '../database/models/MatchModel';
+import { matches, matchesFinished, matchesInProgress } from './mocks/matches.mock';
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Testes do endpoint /matches.', () => {
+  let chaiHttpResponse: Response;
+
+  afterEach(()=>{
+    (Match.findAll as sinon.SinonStub).restore();
+  })
+
+  it('Deve listar todas partidas com sucesso, e retornar um status 200.', async () => {
+    sinon
+      .stub(Model, 'findAll')
+      .resolves(matches as unknown as Match[]);
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches')
+    expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body).to.be.deep.equal(matches);
+  });
+  it('Deve listar partidas em andamento, e retornar um status 200.', async () => {
+    sinon
+      .stub(Model, 'findAll')
+      .resolves(matchesInProgress as unknown as Match[]);
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches?inProgress=true')
+    expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body).to.be.deep.equal(matchesInProgress);
+  });
+  it('Deve listar partidas finalizadas, e retornar um status 200.', async () => {
+    sinon
+      .stub(Model, 'findAll')
+      .resolves(matchesFinished as unknown as Match[]);
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches?inProgress=false')
+    expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body).to.be.deep.equal(matchesFinished);
+  });
+});
+ 
